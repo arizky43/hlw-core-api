5b6a4404a8bee6b7d98be9d58dcd7c2511092fa8
Arizky Nurillahi <arizky.nurillahi@gmail.com>
2025-09-20 23:43:33 +0700

    feat(auht): remove unuse schema

--- src/core/auth/auth.schema.ts
+++ src/core/auth/auth.schema.ts
@@ -18,26 +18,5 @@ export const RefreshTokenPayloadSchema = t.Object({
   }),
 });
 
-export const TokenResponseSchema = t.Object({
-  access_token: t.String({
-    description: "JWT access token for authentication",
-  }),
-  refresh_token: t.String({
-    description: "JWT refresh token for token renewal",
-  }),
-});
-
-export const LoginResponseSchema = t.Object({
-  code: t.Number({
-    description: "Response status code",
-  }),
-  message: t.String({
-    description: "Response message",
-  }),
-  data: TokenResponseSchema,
-});
-
 export type TLoginPayload = typeof LoginPayloadSchema.static;
 export type TRefreshTokenPayload = typeof RefreshTokenPayloadSchema.static;
-export type TTokenResponse = typeof TokenResponseSchema.static;
-export type TLoginResponse = typeof LoginResponseSchema.static;
